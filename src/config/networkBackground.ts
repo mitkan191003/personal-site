ec11b4ac1f41dee8e315fefb019b3313f94cf0dd
mitkan191003 <mithrak8022@gmail.com>
2026-01-30 22:15:11 -0500

    Starting Fresh

--- src/config/networkBackground.ts
+++
@@ -1,5 +0,0 @@
-export const networkBackgroundOptions = {
-  minNetworkSettings: { density: 14000, maxNodes: 100, maxDistance: 160 },
-  maxNetworkSettings: { density: 1000, maxNodes: 2000, maxDistance: 80 },
-  defaultSliderPosition: 0.05,
-} as const;
